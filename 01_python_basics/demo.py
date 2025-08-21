98f576c03e0b858ae1018fdcd16fb77a5275e8e7
ravi2krishna <ravi2krishna@gmail.com>
2025-08-21 08:03:30 +0530

    Basic Python - Rules

---
+++ 01_python_basics/demo.py
@@ -0,0 +1,19 @@
+# comments - ignored by interpreter 
+# a = 10
+# b = 20
+# print(a+b)
+
+# print(hello world) # SyntaxError: invalid syntax
+print(9) # number - int
+print(9.5) # number - float
+a = 10
+print(a)
+greetings = "good morning" # text - string
+print(greetings)
+print('hello world')
+
+
+# import demo
+import keyword
+print(keyword.kwlist)
+
