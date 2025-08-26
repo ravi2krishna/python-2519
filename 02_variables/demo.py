2b63001830652a3ee167d43fc50313b359b87170
ravi2krishna <ravi2krishna@gmail.com>
2025-08-26 08:16:29 +0530

    Data Types in Python

--- 02_variables/demo.py
+++ 02_variables/demo.py
@@ -169,3 +169,50 @@ print(n1 is n2)
 print(n1 == n2)
 
 print(n1 is not n2)
+
+# Data Types 
+num = 10 # int 
+print(type(num))
+      
+num = 10.0 # float 
+print(type(num))
+
+num = 1 + 2j # complex
+print(type(num))
+
+data = "hello" # string
+print(type(data))
+
+data = "h" # string
+print(type(data))
+
+canVote = True
+print(type(canVote))
+
+list_nums = [10,20,30,10]
+list_colors = ["blue","red","green"]
+print(type(list_nums))
+print(type(list_colors))
+print(list_nums)
+
+list_nums = (10,20,30,10)
+print(type(list_nums))
+print(list_nums)
+
+list_nums = {10,20,30,10}
+print(type(list_nums))
+print(list_nums)
+
+list_nums = {"n1":10,"n2":20,"n3":30}
+print(type(list_nums))
+print(list_nums)
+
+x = None
+print(type(x))
+print(x)
+
+class Student: # class
+    pass # skip - do nothing 
+
+edify_student = Student() # object
+print(type(edify_student))
