1520341343bf3cfea21326a276578a7974141099
ravi2krishna <ravi2krishna@gmail.com>
2025-09-10 08:05:02 +0530

    19th Sep - Strings in Python - 3

--- 04_strings/demo.py
+++ 04_strings/demo.py
@@ -174,3 +174,81 @@ print("User Given ID: "+user_given_email)
 print("Gmail Auto Format ID: "+format_email)
 
 # simulate PAN correction -> https://www.pan.utiitsl.com/panonline_ipg/forms/csfPan.html/csfPreForm
+user_given_pan = input("Enter Your PAN ID: ")
+
+if user_given_pan.isalnum():
+    format_pan = user_given_pan.upper()
+    print("User Given PAN: "+user_given_pan)
+    print("PAN Auto Format ID: "+format_pan)
+else:
+    print("User Given PAN is INVALID: "+user_given_pan)    
+
+# check for sub string 
+# simulate email id verification -> name@gmail.com
+user_given_email = input("Enter Your Email ID: ")
+if user_given_email.find("@") != -1:
+    print("Valid Email")
+else:
+    print("InValid Email")
+    
+# above can be achieved using operator also 
+user_given_email = input("Enter Your Email ID: ")
+if "@" in user_given_email:
+    print("Valid Email")
+else:
+    print("InValid Email")
+    
+# redirect call based on given isd code
+# https://us1.discourse-cdn.com/flex016/uploads/weweb/original/2X/d/dbe25afb4aeb05640347e2f7c1b7ae532ebb28f2.png
+phone_number = input("Enter Your Phone Number with ISD CODE: ")
+if phone_number.startswith("+91"):
+    print("Call Connected To INDIA")
+elif phone_number.startswith("+86"):
+    print("Call Connected To CHINA")
+elif phone_number.startswith("+33"):
+    print("Call Connected To FRANCE")
+else:
+    print("ISD Available Only To INDIA, CHINA & FRANCE")
+
+# Check if email synchronization is possible or not
+source_email = input("Enter Your Source Email ID: ")
+dest_email = input("Enter Your Destination Email ID To SYNC: ")
+if source_email.endswith("@gmail.com") and dest_email.endswith("@gmail.com"):
+    print("synchronizing")
+else:
+    print("Both Emails should be from same providers")
+
+# simulate gmail functionality -> remove spaces from input
+user_given_email = input("Enter Your Email ID: ")
+format_email = user_given_email.strip()
+print("User Given ID: "+user_given_email)
+print("Gmail Auto Format ID: "+format_email)
+
+# Read Data From CSV File
+
+# sample data taken from a CSV file
+# "Name,City,Age,Email,Role"
+csv_line = "John,Hyd,20,john@gmail.com,developer"
+
+# Original Data
+print("Original Line: ",csv_line)
+
+parsed_fields = csv_line.split(",")
+
+# Parsed Data
+print("Parsed Data: ",parsed_fields)
+
+# Access Name & Role 
+print("Name: ",parsed_fields[0])
+print("Role: ",parsed_fields[4])
+
+# VS Code Find & Replace / OTP template / Order ID Template
+email_template = "Hello User, your order #{order_id} has been shipped"
+
+order_id = "OD-ID-9090"
+
+# Replacing placeholder with values
+personalized_email = email_template.replace("{order_id}",order_id)
+
+# sending email
+print(personalized_email)
